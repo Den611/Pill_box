25f853671235ea85ba487733af7647fba9515db6
Den Herasymchuk <d.gerasim4uk@gmail.com>
2026-04-14 18:46:12 +0300

    батнік

--- start.bat
+++ start.bat
@@ -8,14 +8,18 @@ echo ========================================
 echo [INFO] Запуск ngrok у новому вікні (порт 8080)...
 start "Ngrok" cmd /k "ngrok http 8080"
 
-:: Перевіряємо чи існує папка віртуального оточення
-if exist ".venv\Scripts\python.exe" (
-    echo [OK] Знайдено віртуальне оточення. Запускаємо...
-    .\.venv\Scripts\python.exe main.py
-) else (
-    echo [WARNING] Віртуальне оточення (.venv) не знайдено!
-    echo Спроба запуску через глобальний python...
-    python main.py
-)
+if not exist ".venv\Scripts\python.exe" goto global_python
 
+echo [OK] Знайдено віртуальне оточення. Запускаємо...
+.\.venv\Scripts\python.exe main.py
+goto end_script
+
+:global_python
+echo [WARNING] Віртуальне оточення (.venv) не знайдено!
+echo Спроба запуску через глобальний python...
+python main.py
+
+:end_script
+echo.
+echo Робота сервера завершена або сталася помилка!
 pause
