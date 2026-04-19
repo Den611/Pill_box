03200ca205bd0ebc78c44e56d28ea7b45d80baf9
Den Herasymchuk <d.gerasim4uk@gmail.com>
2026-04-19 20:32:55 +0300

    ,

--- start.bat
+++ start.bat
@@ -1,26 +1,26 @@
 @echo off
-chcp 65001
 title PillBox Server
+chcp 65001 > nul
 cd /d "%~dp0"
 echo ========================================
-echo   Запуск сервера PillBox...
+echo   Starting PillBox Server...
 echo ========================================
 
-echo [INFO] Запуск ngrok у новому вікні (порт 8080)...
+echo [INFO] Starting ngrok in a new window (port 8080)...
 start "Ngrok" cmd /k "ngrok http 8080"
 
 if not exist ".venv\Scripts\python.exe" goto global_python
 
-echo [OK] Знайдено віртуальне оточення. Запускаємо...
+echo [OK] Virtual environment found. Starting...
 .\.venv\Scripts\python.exe main.py
 goto end_script
 
 :global_python
-echo [WARNING] Віртуальне оточення (.venv) не знайдено!
-echo Спроба запуску через глобальний python...
+echo [WARNING] Virtual environment (.venv) not found!
+echo Attempting to start via global python...
 python main.py
 
 :end_script
 echo.
-echo Робота сервера завершена або сталася помилка!
+echo Server stopped!
 pause
